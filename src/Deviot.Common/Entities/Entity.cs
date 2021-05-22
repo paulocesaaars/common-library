97ef28ed4ffb06f052451785ddf9ff83763765ab
Paulo César de Souza <paulo.souza@deviot.com.br>
2021-05-21 23:18:19 -0300

    Primeiro commit

---
+++ src/Deviot.Common/Entities/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Deviot.Common.Entities
+{
+    public class Entity
+    {
+        public Guid Id { get; protected set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Entity(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
