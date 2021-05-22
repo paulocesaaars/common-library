34d5944d620ca57309bba6800b8cfe9c97a7b2b8
Paulo César de Souza <paulo.souza@deviot.com.br>
2021-05-21 23:28:12 -0300

    Adicionado projeto de repositorio para Entity com postgres

---
+++ src/Deviot.Common.Repository/EntityMapping.cs
@@ -0,0 +1,18 @@
+﻿using Deviot.Common.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Deviot.Common.Repository
+{
+    public abstract class EntityMapping
+    {
+        protected static void ConfigureBase<TEntity>(EntityTypeBuilder<TEntity> builder, string tableName) where TEntity : Entity
+        {
+            builder.ToTable(tableName);
+
+            builder.HasKey(t => t.Id);
+
+            builder.HasIndex(t => new { t.Id });
+        }
+    }
+}
