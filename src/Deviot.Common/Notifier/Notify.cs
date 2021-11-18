73feb3fe077eb8e84a9187d38d4ccf379d14e390
paulo <paulocesaaars@gmail.com>
2021-11-17 23:47:59 -0300

    Adicionado id de criação do registro

--- src/Deviot.Common/Notifier/Notify.cs
+++ src/Deviot.Common/Notifier/Notify.cs
@@ -1,17 +1,28 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Net;
 
 namespace Deviot.Common
 {
+    [ExcludeFromCodeCoverage]
     public class Notify
     {
-        [ExcludeFromCodeCoverage]
+        public string Id { get; private set; }
+
         public HttpStatusCode Type { get; private set; }
 
         public string Message { get; private set; }
 
         public Notify(HttpStatusCode type, string message)
         {
+            Id = Guid.NewGuid().ToString();
+            Type = type;
+            Message = message;
+        }
+
+        public Notify(string id, HttpStatusCode type, string message)
+        {
+            Id = id;
             Type = type;
             Message = message;
         }
